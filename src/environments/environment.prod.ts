aba397347515dbd7fcac259a7f6e3291a3cf4e69
pronos source <info@pronos.ar>
2024-04-22 16:46:13 -0300

    fix prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  MARVEL_API:'https://gateway.marvel.com:443/v1/public/characters'
 };
+
+export const API_KEY = 'f98703d01a701161b7107c57653729ac'
